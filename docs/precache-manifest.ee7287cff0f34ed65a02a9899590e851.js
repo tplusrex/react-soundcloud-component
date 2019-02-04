f7c751bd776af56a71db7b1ac4aac28e7b981adb
Tony Vongprachanh <tvongpr@gmail.com>
2019-02-03 23:31:27 -0800

    Create doc folder

---
+++ docs/precache-manifest.ee7287cff0f34ed65a02a9899590e851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "73e7285ed86343d9789f",
+    "url": "/react-practice/static/js/main.73e7285e.chunk.js"
+  },
+  {
+    "revision": "dd08381d7af833a92c5e",
+    "url": "/react-practice/static/js/1.dd08381d.chunk.js"
+  },
+  {
+    "revision": "73e7285ed86343d9789f",
+    "url": "/react-practice/static/css/main.250e2dee.chunk.css"
+  },
+  {
+    "revision": "aa2233af34190f4a72b59ce1cb35e10e",
+    "url": "/react-practice/index.html"
+  }
+];
